15e02bd3b1f90fe7d66f4731229bc4bae491cc6d
lijian <lijian@topscomm.com>
2021-02-22 15:56:56 +0800

    first commit

---
+++ 黑白棋/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace 黑白棋
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.BurlyWood;
+            this.MainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainPanel.ErrorImage = null;
+            this.MainPanel.InitialImage = null;
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(400, 400);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.TabStop = false;
+            this.MainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 424);
+            this.Controls.Add(this.MainPanel);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "黑白棋";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPanel;
+    }
+}
+
---
+++ 黑白棋/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+namespace 黑白棋
+{
+    public partial class MainForm : Form
+    {
+        bool Flag = true;
+        Graphics mGraphics = null;
+        Brush Black = Brushes.Black;
+        Brush White = Brushes.White;
+        Type[,] mType = new Type[10, 10];
+        Pen mPen = new Pen(Color.Black);
+        Rectangle mEllipse = new Rectangle();
+        Bitmap mBitmap = new Bitmap(398, 398);
+        private enum Type : byte
+        {
+            Empty, White, Black
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+            mEllipse.Width = 32;
+            mEllipse.Height = 32;
+            MainPanel.Image = mBitmap;
+            mGraphics = Graphics.FromImage(MainPanel.Image);
+            mGraphics.PixelOffsetMode = (PixelOffsetMode)2;
+            mGraphics.SmoothingMode = (SmoothingMode)2;
+            RefreshScreen();
+        }
+        private void RefreshScreen()
+        {
+            MainPanel.Image = mBitmap;
+            mGraphics.Clear(Color.BurlyWood);
+            for (int i = 39; i < 360; i += 40)
+            {
+                mGraphics.DrawLine(mPen, 0, i, 398, i);
+                mGraphics.DrawLine(mPen, i, 0, i, 398);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    mEllipse.X = i * 40 + 3;
+                    mEllipse.Y = j * 40 + 3;
+                    bool bWhite = mType[i, j] == Type.White;
+                    bool bBlack = mType[i, j] == Type.Black;
+                    if (bWhite) mGraphics.FillEllipse(White, mEllipse);
+                    if (bBlack) mGraphics.FillEllipse(Black, mEllipse);
+                }
+            }
+        }
+        private void MainPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mType[e.X / 40, e.Y / 40] == Type.Empty)
+            {
+                Type T = Flag ? Type.Black : Type.White;
+                mType[e.X / 40, e.Y / 40] = T;
+                Flag = !Flag;
+                RefreshScreen();
+            }
+        }
+    }
+}
